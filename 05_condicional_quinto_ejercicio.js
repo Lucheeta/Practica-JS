ccfe7830f6a7ac8a2abb34abbeb1693616bb9d3c
Luciana Lucero <lucianalucero@gmail.com>
2023-05-02 18:09:26 -0300

    Completé ejercicio 5 de JS

--- 05_condicional_quinto_ejercicio.js
+++ 05_condicional_quinto_ejercicio.js
@@ -4,4 +4,20 @@ const MIN_EDAD = 1;
 const MAX_EDAD = 120;
 
 let edad = parseInt(prompt(`Ingresá tu edad (entre ${MIN_EDAD} y ${MAX_EDAD})`));
-let genero = prompt("Ingresá tu género [M o F]");
+
+if (edad >= MIN_EDAD && edad <= MAX_EDAD ) {
+    let genero = prompt("Ingresá tu género [M o F]");
+    
+    if (genero === "M" || genero === "F" || genero === "m" || genero === "f") {
+        if (edad >= 65 || (genero === "F" && edad >=60)) {
+            console.log("Puede jubilarse");
+        } else {
+            console.log("No puede jubilarse");
+        }
+    } else {
+        console.log("Género inválido");
+    }
+    
+} else {
+    console.log("Edad inválida"); 
+}
