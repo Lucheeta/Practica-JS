405c85d66e6671629176d8165eb8dd09c8bd21c2
Luciana Lucero <lucianalucero@gmail.com>
2023-05-08 01:31:10 -0300

    Nuevos ejercicios

--- 04_condicional_cuarto_ejercicio.js
+++ 04_condicional_cuarto_ejercicio.js
@@ -1,13 +1,16 @@
 // Realizá un programa que permita al usuario ingresar tres números enteros. La computadora debe indicar cuál de ellos es el mayor.
 
+let respuesta;
 let primerNumero = parseInt(prompt("Ingrese el primer número"));
 let segundoNumero = parseInt(prompt("Ingrese el segundo número"));
 let tercerNumero = parseInt(prompt("Ingrese el tercer número"));
 
-if (primerNumero > segundoNumero && tercerNumero) {
-	console.log(`El ${primerNumero} es mayor`);
-} if (segundoNumero > primerNumero && tercerNumero) {
-	console.log(`El ${segundoNumero} es mayor`)
-} if (tercerNumero > primerNumero && segundoNumero) {
-	console.log(`El ${tercerNumero} es mayor`);
+if (primerNumero > segundoNumero && primerNumero > tercerNumero) {
+	respuesta = (`${primerNumero}`);
+} if (segundoNumero > primerNumero && segundoNumero > tercerNumero) {
+	respuesta = (`${segundoNumero}`);
+} if (tercerNumero > primerNumero && tercerNumero > segundoNumero) {
+	respuesta = (`${tercerNumero}`);
 	}
+	
+console.log(`El ${respuesta} es mayor`);
