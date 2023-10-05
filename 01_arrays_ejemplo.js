28cdc8bc6cf3b134fe72ce97a36f384161fa5bc0
Luciana Lucero <lucianalucero@gmail.com>
2023-10-05 17:48:32 -0300

    algunos cambios en el archivo de arrays

--- 01_arrays_ejemplo.js
+++ 01_arrays_ejemplo.js
@@ -1,4 +1,15 @@
 let frutas = ["Manzana", "Banana"]
 
  
-console.log(frutas.length);
+//console.log(frutas.length);
+
+frutas.push("pera")
+
+//console.log(frutas.length);
+
+for (let index = 0; index < frutas.length; index++) {
+    const element = frutas[index];
+   
+    console.log(frutas.toUpperCase());
+
+}
